7dc1aa6aa96a31fe8b2b4c46034804a33d471510
bob44240 <bob44240@users.noreply.github.com>
2022-07-12 12:45:57 -0400

    Link lib1 to app

--- etest1/src/app/app.module.ts
+++ etest1/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Lib1Module } from 'lib1';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, Lib1Module],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
